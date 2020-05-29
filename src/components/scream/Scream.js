6d39d68d73355b79d76ed9cbd37bcc054bf32791
Ajay Duth <ajayduth123@gmail.com>
2020-05-29 18:01:27 +0530

    Almost completed.no notification button

---
+++ src/components/scream/Scream.js
@@ -0,0 +1,121 @@
+import React, { Component, Fragment } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import MyButton from "../../utils/MyButton";
+import DeleteScream from "./DeleteScream";
+
+import ChatIcon from "@material-ui/icons/Chat";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { likeScream, unlikeScream } from "../../redux/actions/dataAction";
+import ScreamDialog from "./ScreamDialog";
+import LikeButton from "./LikeButton";
+const styles = {
+  card: {
+    display: "flex",
+    marginBottom: 20,
+    position: "relative",
+  },
+  image: {
+    minWidth: 200,
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover",
+  },
+};
+
+class Scream extends Component {
+  render() {
+    dayjs.extend(relativeTime);
+
+    const {
+      classes,
+      scream: {
+        body,
+        createdAt,
+        userImage,
+        userHandle,
+        screamId,
+        likeCount,
+        commentCount,
+      },
+      user: {
+        authenticated,
+        credentials: { handle },
+      },
+    } = this.props;
+
+    const deleteButton =
+      authenticated && userHandle == handle ? (
+        <DeleteScream screamId={screamId} />
+      ) : null;
+
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile image"
+          className={classes.image}
+        />
+        <CardContent className={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          {deleteButton}
+          <Typography variant="body2" color="textSecondary">
+            {dayjs(createdAt).fromNow()}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
+          <LikeButton screamId={screamId} />
+          {likeCount} Likes
+          <MyButton tip="comments">
+            <ChatIcon color="primary" />
+          </MyButton>
+          <span>{commentCount} Comments</span>
+          <ScreamDialog
+            screamId={screamId}
+            userHandle={userHandle}
+            openDialog={this.props.openDialog}
+          />
+        </CardContent>
+      </Card>
+    );
+  }
+}
+
+Scream.propTypes = {
+  user: PropTypes.object.isRequired,
+  scream: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+  openDialog: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapActionsToProps = {
+  likeScream,
+  unlikeScream,
+};
+
+export default connect(
+  mapStateToProps,
+  mapActionsToProps
+)(withStyles(styles)(Scream));
---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,99 @@
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_UNAUTHENTICATED,
+  LOADING_USER,
+  MARK_NOTIFICATIONS_READ,
+} from "../types";
+
+import Axios from "axios";
+
+export const loginUser = (userData, history) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  Axios.post("/login", userData)
+    .then((res) => {
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch((err) => {
+      // console.log(err.response.data);
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
+
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem("FBIdToken");
+  delete Axios.defaults.headers.common["Authorization"];
+  dispatch({ type: SET_UNAUTHENTICATED });
+};
+
+export const getUserData = () => (dispatch) => {
+  dispatch({ type: LOADING_USER });
+  Axios.get("/user")
+    .then((res) => {
+      dispatch({
+        type: SET_USER,
+        payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+export const signupUser = (newUserdata, history) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  Axios.post("/signup", newUserdata)
+    .then((res) => {
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch((err) => {
+      // console.log(err.response.data);
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
+
+export const uploadImage = (formData) => (dispatch) => {
+  dispatch({ type: LOADING_USER });
+  Axios.post("/user/image", formData)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch((err) => console.log(err));
+};
+
+export const editUserDetails = (userDetails) => (dispatch) => {
+  dispatch({ type: LOADING_USER });
+  Axios.post("/user", userDetails)
+    .then(() => {
+      dispatch(getUserData());
+    })
+    .catch((err) => console.log(err));
+};
+
+export const markNotificationsRead = (notificationIds) => (dispatch) => {
+  Axios.post("/notifications", notificationIds)
+    .then((res) => {
+      dispatch({
+        type: MARK_NOTIFICATIONS_READ,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+const setAuthorizationHeader = (token) => {
+  const FBIdToken = `Bearer ${token}`;
+  localStorage.setItem("FBIdToken", FBIdToken);
+  Axios.defaults.headers.common["Authorization"] = FBIdToken;
+};
